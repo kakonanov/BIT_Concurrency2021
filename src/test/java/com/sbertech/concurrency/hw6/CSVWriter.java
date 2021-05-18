627923c261b41c8932a7b77e7b0bdc7ea09e1ccb
kakonanov <ka.konanov@gmail.com>
2021-05-19 01:00:57 +0300

    hw 6 v2

---
+++ src/test/java/com/sbertech/concurrency/hw6/CSVWriter.java
@@ -0,0 +1,39 @@
+package com.sbertech.concurrency.hw6;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CSVWriter {
+    private final Path path;
+
+    public CSVWriter(Path path) {
+        this.path = path;
+        printColumns();
+    }
+
+    private void printColumns() {
+        try {
+            Files.write(path, Collections.singleton("load,size,queueSize,pocketNum,value"), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write(double load, int size, int queueSize, int pocketNum, long[] array) {
+        String param = load + "," + size + "," + queueSize + "," + pocketNum;
+        Arrays.stream(array)
+                .forEach(l -> {
+                    try {
+                        Files.write(path, Collections.singleton(param + "," + l), StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+}
--- src/test/java/com/sbertech/concurrency/hw6/TokenRingTest.java
+++ src/test/java/com/sbertech/concurrency/hw6/TokenRingTest.java
@@ -5,43 +5,66 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 class TokenRingTest {
 	private static final int latencyArraySize = 100;
 	private static final long millisForWork = 10_000; //milliseconds
 	private static final long periodTime = 500; // milliseconds
-	private static final int throughputArraySize = (int) ((millisForWork - 2_000) / periodTime);
+	private static final int throughputArraySize = (int) ((millisForWork - 3_000) / periodTime);
+
+	private static final Path latencyPath = Paths.get("src/test/resources/latency.csv");
+	private static final Path throughputPath = Paths.get("src/test/resources/throughput.csv");
+
+	private static final CSVWriter latencyWriter = new CSVWriter(latencyPath);
+	private static final CSVWriter throughputWriter = new CSVWriter(throughputPath);
+
+
 
 
 	@BeforeAll
-	static void before() throws InterruptedException {
+	static void before() throws InterruptedException, FileNotFoundException {
 		int size = 2;
 		int queueSize = 2;
 		TokenRing tokenRing = TokenRing.create(size, queueSize, latencyArraySize, throughputArraySize, periodTime);
 		List<NodeConnector> nodeConnectorList = tokenRing.getNodeConnectors();
 		nodeConnectorList.get(0).put(new Token(0));
 		tokenRing.start();
-		Thread.sleep(millisForWork);
+		Thread.sleep(5_000);
 		tokenRing.stop();
 	}
 
 	@ParameterizedTest
-	@ValueSource(doubles = {0.15, 0.5, 0.8})
-	public void test(double load) throws InterruptedException {
-		System.out.println("Load: " + load);
-//		for (int size = 2; size <= 6; ++size) {
-//			for (int queueSize = 2; queueSize <= 6; ++queueSize) {
-//				testTokenRing(size, queueSize, size * queueSize * load);
-//			}
-//		}
-		testTokenRingToCheck(3, 4, 8);
-		System.out.println("------------------------------\n\n");
+	@ValueSource(ints = {2, 3, 4, 5, 6})
+	public void testLowLoad(int size) throws InterruptedException {
+		double load = 0.2;
+		for (int queueSize = 2; queueSize <= size + 2; ++queueSize) {
+			testTokenRing(load, size, queueSize, (int) Math.ceil(size * queueSize * load));
+		}
 	}
 
-	public void testTokenRingToCheck(int size, int queueSize, double pocketNum) throws InterruptedException {
-		System.out.println("Size: " + size + " , queueSize: " +  queueSize);
+	@ParameterizedTest
+	@ValueSource(ints = {2, 3, 4, 5, 6})
+	public void testMediumLoad(int size) throws InterruptedException {
+		double load = 0.5;
+		for (int queueSize = 2; queueSize <= size + 2; ++queueSize) {
+			testTokenRing(load, size, queueSize, (int) Math.ceil(size * queueSize * load));
+		}
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {2, 3, 4, 5, 6})
+	public void testHighLoad(int size) throws InterruptedException {
+		double load = 0.8;
+		for (int queueSize = 2; queueSize <= size + 2; ++queueSize) {
+			testTokenRing(load, size, queueSize, (int) Math.ceil(size * queueSize * load));
+		}
+	}
+
+	public void testTokenRing(double load, int size, int queueSize, int pocketNum) throws InterruptedException {
 		TokenRing tokenRing = TokenRing.create(size, queueSize, latencyArraySize, throughputArraySize, periodTime);
 		List<NodeConnector> nodeConnectorList = tokenRing.getNodeConnectors();
 		for (int i = 0; i < pocketNum; ++i) {
@@ -50,22 +73,9 @@ class TokenRingTest {
 		tokenRing.start();
 		Thread.sleep(millisForWork);
 		tokenRing.stop();
-		System.out.println(Arrays.toString(tokenRing.getLatencies()));
-		System.out.println(Arrays.toString(tokenRing.getThroughputs()));
-	}
 
-	public void testTokenRing(int size, int queueSize, double pocketNum) throws InterruptedException {
-		System.out.println("Size: " + size + " , queueSize: " +  queueSize);
-		TokenRing tokenRing = TokenRing.create(size, queueSize, latencyArraySize, throughputArraySize, periodTime);
-		List<NodeConnector> nodeConnectorList = tokenRing.getNodeConnectors();
-		for (int i = 0; i < pocketNum; ++i) {
-			nodeConnectorList.get(i % size).put(new Token(i));
-		}
-		tokenRing.start();
-		Thread.sleep(millisForWork);
-		tokenRing.stop();
-		System.out.println(Arrays.toString(tokenRing.getLatencies()));
-		System.out.println(Arrays.toString(tokenRing.getThroughputs()));
+		latencyWriter.write(load, size, queueSize, pocketNum, tokenRing.getLatencies());
+		throughputWriter.write(load, size, queueSize, pocketNum, tokenRing.getThroughputs());
 	}
 
 }
